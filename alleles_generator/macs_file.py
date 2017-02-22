ddfcb1c1399aee36ccf19e1f83e42251ec46eab2
Ariella Gladstein <agladstein@email.arizona.edu>
2017-02-22 12:28:16 -0700

    added function for alleles bitarray and test

--- alleles_generator/macs_file.py
+++ alleles_generator/macs_file.py
@@ -1,4 +1,7 @@
 import re
+from bitarray import bitarray
+from sys import getsizeof
+
 
 
 class AllelesMacsFile(object):
@@ -18,4 +21,17 @@ class AllelesMacsFile(object):
                 site_alleles = list(columns[4].strip())
                 alleles.append(site_alleles)
         macs_file.close()
+        print 'size list '+str(getsizeof(alleles))
         return alleles
+
+    def make_bitarray(self):
+        macs_file = open(self.macs_file_name, 'r')
+        alleles_bits = bitarray()
+        for line in macs_file:
+            if re.match('SITE', line):
+                columns = line.split('\t')
+                site_alleles = columns[4].strip()
+                alleles_bits.extend(site_alleles)
+        macs_file.close()
+        print 'size bits '+str(getsizeof(alleles_bits))
+        return alleles_bits
