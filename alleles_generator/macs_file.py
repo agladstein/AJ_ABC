5c5165031d5c004631aa884a1884797890629a5b
Ariella Gladstein <agladstein@email.arizona.edu>
2017-02-22 15:03:38 -0700

    create and test bitarray seq for each population

--- alleles_generator/macs_file.py
+++ alleles_generator/macs_file.py
@@ -5,14 +5,14 @@ from sys import getsizeof
 
 
 class AllelesMacsFile(object):
-    """Make list of lists containing alleles from macs sites output file.
-    macs sites output file is made by
-    macs 20 1000 -t 0.001 -r 0.0004 -s 100000 -h 1e5 -I 2 10 10 -ej 0.0025 1 2 >trees.txt 1> sites.txt"""
-
     def __init__(self, macs_file_name):
         self.macs_file_name = macs_file_name
 
     def make_lists(self):
+        """Make list of lists containing alleles from macs sites output file.
+        macs sites output file is made by
+        macs 20 1000 -t 0.001 -r 0.0004 -s 100000 -h 1e5 -I 2 10 10 -ej 0.0025 1 2 >trees.txt 1> sites.txt"""
+
         alleles = []
         macs_file = open(self.macs_file_name, 'r')
         for line in macs_file:
@@ -21,10 +21,12 @@ class AllelesMacsFile(object):
                 site_alleles = list(columns[4].strip())
                 alleles.append(site_alleles)
         macs_file.close()
-        print 'size list '+str(getsizeof(alleles))
         return alleles
 
     def make_bitarray(self):
+        """Make bitarray containing alleles from macs sites output file.
+        macs sites output file is made by
+        macs 20 1000 -t 0.001 -r 0.0004 -s 100000 -h 1e5 -I 2 10 10 -ej 0.0025 1 2 >trees.txt 1> sites.txt"""
         macs_file = open(self.macs_file_name, 'r')
         alleles_bits = bitarray()
         for line in macs_file:
@@ -33,5 +35,19 @@ class AllelesMacsFile(object):
                 site_alleles = columns[4].strip()
                 alleles_bits.extend(site_alleles)
         macs_file.close()
-        print 'size bits '+str(getsizeof(alleles_bits))
         return alleles_bits
+
+    def make_bitarray_seq(self, n_0, n_m):
+        """Make bitarray containing alleles from macs sites output file for one population (equivalent of seq lists).
+        macs sites output file is made by
+        macs 20 1000 -t 0.001 -r 0.0004 -s 100000 -h 1e5 -I 2 10 10 -ej 0.0025 1 2 >trees.txt 1> sites.txt"""
+        macs_file = open(self.macs_file_name, 'r')
+        seq_bits = bitarray()
+        for line in macs_file:
+            if re.match('SITE', line):
+                columns = line.split('\t')
+                site_alleles = columns[4].strip()
+                for i in xrange(n_0,n_m):
+                    seq_bits.extend(site_alleles[i])
+        macs_file.close()
+        return seq_bits
