467b7580478518e9b894f2b95ffe23a9ded46dc7
Ariella Gladstein <agladstein@email.arizona.edu>
2017-02-22 17:25:31 -0700

    Fixed test for main function. Alleles appear to be the same

--- tests/test_main_function_AJmodel1_chr1.py
+++ tests/test_main_function_AJmodel1_chr1.py
@@ -5,7 +5,7 @@ from main_function_AJmodel1_chr1 import main
 
 class MyMain(unittest.TestCase):
     def test_basic(self):
-        arguments  = ['',1,'ill_650_test.bed',1000000,1234567,'rand',1]
+        arguments  = ['',1,'ill_650_test.bed',1000,1278,'prior',1]
         results = main(arguments)
         print results
 
@@ -14,12 +14,12 @@ class MyMain(unittest.TestCase):
             import json
             import time
             with open('tests/test_data/my_main_test_basic_result_{}.json'.format(time.time()), 'w') as fp:
-                json.dump(results_to_dump, fp, indent=2)
+                json.dump(results_to_dump, fp, indent=2, ensure_ascii=True)
 
-        # dump_results(results)
+        #dump_results(results)
 
-        with open('tests/test_data/my_main_test_basic_result_1487746810.95.json', 'r') as fp:
-            expected_results = json.load(fp)
+        with open('tests/test_data/my_main_test_basic_result_1487808470.62.json', 'r') as fp:
+            expected_results = json.load(fp, encoding='ascii')
 
         self.maxDiff = None
         self.assertListEqual(results, expected_results)
