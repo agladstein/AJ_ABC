bac8b31e48ea26a887d88adf0d3c29c6b6123ea0
Ariella Gladstein <agladstein@email.arizona.edu>
2017-03-01 11:46:17 -0700

    added pseudo array with lists

--- test_main_function_AJmodel1_chr1_macs_file.py
+++ test_main_function_AJmodel1_chr1_macs_file.py
@@ -3,6 +3,7 @@ from alleles_generator.macs_file import AllelesMacsFile
 from bitarray import bitarray
 from summary_statistics import afs_stats
 from summary_statistics import afs_stats_bitarray
+from ascertainment.pseudo_array import find2, add_snps
 import itertools
 
 
@@ -14,6 +15,8 @@ def main():
     Must give list or bitarray as argument"""
 
     option = argv[1]
+
+    length = 249163442
 
     naf_CGI = 18
     neu_CGI = 18
@@ -36,6 +39,7 @@ def main():
     asc_nb_af = 10
     asc_nb_eu = 16
     asc_nb_as = 8
+    daf = 0.05
 
     total_naf = naf_CGI + asc_nb_af
     total_neu = neu_CGI + asc_nb_eu
@@ -47,8 +51,14 @@ def main():
 
     if option == 'list':
         alleles_macs_file = AllelesMacsFile('tests/test_data/sites1000000.txt')
-        alleles = alleles_macs_file.make_lists()
+        alleles = alleles_macs_file.make_lists()[0]
         nbss = len(alleles)
+
+        pos = []
+        sim_position = alleles_macs_file.make_lists()[1]
+        for i in xrange(nbss):
+            position = round(float(sim_position[i]) * (float(length)))
+            pos.append(position)
 
         #########get data from the simulations
         Talleles = zip(*alleles)
@@ -73,6 +83,125 @@ def main():
         asc_panel.extend(seqAf_ds)
         asc_panel.extend(seqEu_ds)
         asc_panel.extend(seqAs_ds)
+
+        Tasc_panel = zip(*asc_panel)
+        print 'number of sites in Tasc_panel:', len(Tasc_panel)
+        print 'number of chromosomes in Tasc_panel:', len(Tasc_panel[0])
+
+        #######Array with the available sites given the frequency cut off
+        ##array with the frequency of all the simulated snps
+        sites_freq = []
+        ##array with the available sites, that pass the frequency cut-off
+        avail_sites = []  ##this one has the positions of the snps
+        index_avail_sites = []  ##this one has the indexes of the snps
+
+        ####Get the positions of the SNPs that are on the chip
+        snp_file = 'ill_650_test.bed'  # SNP file
+        fileSNP = open(snp_file, 'r')
+        SNP = []
+        for line in fileSNP:
+            SNP.append(line)
+        fileSNP.close()
+
+        ###get sites from snp array
+        snps = []
+        for line_snp in SNP:
+            columns = line_snp.split('\t')
+            snps.append(int(columns[2]))
+
+        for n in xrange(len(Tasc_panel)):
+            freq_site = float(Tasc_panel[n][0:len(asc_panel)].count('1')) / float(len(asc_panel))
+            if freq_site >= daf and freq_site <= 1 - daf:
+                sites_freq.append(freq_site)
+                avail_sites.append(pos[n])
+                index_avail_sites.append(n)
+
+        nb_avail_sites = len(avail_sites)
+
+        if (len(avail_sites) == len(snps)):
+            print "number of avail_sites is equal to the number of Array snps"
+            pos_asc = []
+            pos_asc = index_avail_sites
+            nbss_asc = len(pos_asc)
+            flag_nb_asc_snps = 1
+
+
+        elif (len(avail_sites) > len(snps)):
+            print "number of avail_sites greater than number of Array snps"
+            pos_asc = [None] * int(len(snps))  ##indexes of the SNPs that pass the frequency cut-off and position
+            for i in xrange(len(snps)):  # each snp on the snp array on a chromosome
+                ## y is the position of the SNPs in the array
+                y = snps[i]
+                ##find the closest SNP in the array
+                closestleft = find2(avail_sites, y)
+
+                if (i > 0 and pos_asc[i - 1] == closestleft and closestleft + 1 < len(avail_sites)):  ##avoid duplicates
+                    closestleft = closestleft + 1  ##move one position to the right
+                    pos_asc[i] = closestleft
+
+                elif (i > 0 and pos_asc[i - 1] > closestleft and pos_asc[i - 1] + 1 < len(avail_sites)):
+                    closestleft = pos_asc[i - 1] + 1
+                    pos_asc[i] = closestleft
+
+                else:
+                    pos_asc[i] = closestleft
+                    ###if I have duplicates at this point, it means that there were not anyt more snps to choose from
+                    ###closestleft+1 or pos_asc[i-1]+1 == len(avail_sites)
+
+            #####smoothing
+            ##last index of the pos_asc
+            i = len(pos_asc) - 1
+
+            ##check if there is another position that might work better
+            for j in xrange(0, i):
+                if (j == i - 1 and pos_asc[j] + 1 < pos_asc[j + 1] and pos_asc[j] < (len(avail_sites) - 1) and (
+                            j + 1) < len(avail_sites)):
+                    d1 = abs(snps[j] - avail_sites[pos_asc[j]])
+                    d2 = abs(snps[j] - avail_sites[pos_asc[j] + 1])
+                    if (d2 < d1):
+                        pos_asc[j] = pos_asc[j] + 1
+
+            ##removes duplicates
+            pos_asc = (list(set(pos_asc)))
+            pos_asc.sort()
+
+            nbss_asc = len(pos_asc)
+
+            if (len(snps) == nbss_asc):
+                flag_nb_asc_snps = 1
+                print 'nb of asc snps equal to nb array snps'
+
+            if (len(snps) != len(pos_asc)):
+                flag_nb_asc_snps = 0
+                print 'nb of asc snps not equal to nb array snps'
+
+                diff = int(len(snps) - len(pos_asc))
+
+                for m in xrange(1, diff + 1):
+                    pos_asc2 = []
+                    pos_asc2 = add_snps(avail_sites, nb_avail_sites, pos_asc, nbss_asc, nb_array_snps)
+                    pos_asc = pos_asc2
+                    nbss_asc = len(pos_asc)
+
+                    if nbss_asc == len(snps):
+                        flag_nb_asc_snps = 1
+                        break
+
+                    else:
+                        flag_nb_asc_snps = 0
+
+            if (
+                flag_nb_asc_snps == 0):  ##it means that the 1st index in pos_asc is 0; and the last is len(avail_sites)-1
+                diff = int(len(snps) - len(pos_asc))
+                while (len(pos_asc) != len(snps)):
+                    rand_numb = randint(0, len(avail_sites) - 1)
+                    # print 'random',rand_numb
+                    if rand_numb not in pos_asc:
+                        pos_asc.append(rand_numb)
+                pos_asc.sort()
+                nbss_asc = len(pos_asc)
+
+        print 'finished making pseudo array'
 
         res = []
         Af_res = []
@@ -104,6 +233,7 @@ def main():
         res.append(afs_stats.FST2(seqAfCGI, pi_AfCGI, naf_CGI, seqAsCGI, pi_AsCGI, nas_CGI))
         res.append(afs_stats.FST2(seqEuCGI, pi_EuCGI, neu_CGI, seqAsCGI, pi_AsCGI, nas_CGI))
         head = head + 'FST_AfEu_CGI\tFST_AfAs_CGI\tFST_EuAs_CGI\t'
+
 
         return res
 
