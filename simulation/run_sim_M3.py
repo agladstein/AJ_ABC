42faaa6ff3e580128a2b8157c73a9b2b570191cb
Ariella Gladstein <agladstein@email.arizona.edu>
2017-03-10 09:13:01 -0700

    fixed macs commands in cases

--- simulation/run_sim_M3.py
+++ simulation/run_sim_M3.py
@@ -79,19 +79,10 @@ def run_sim(parameters,case,length,chr_number,total,total_naf,total_nas,total_ne
                          str(scaled_Tgrowth_Af), '1', str(scaled_NANC)]
 
         if case == 3:
-            macs_args = ['./bin/macs', str(total), str(length), '-t', str(macs_theta),'-s',str(seed_option), '-r', str(macs_rho), '-h', '1e5',
-                         '-R', 'genetic_map_b37/genetic_map_GRCh37_chr' + str(chr_number) + '.txt.macshs', '-I', '7',
-                         str(total_naf), str(total_nas), str(total_neu), str(nJ), str(nM), str(nEA), str(nWA), '-n',
-                         '1', str(scaled_NAF), '-n', '2', str(scaled_NCHB), '-n', '3', str(scaled_NCEU), '-n', '4',
-                         str(scaled_NJ), '-n', '5', str(scaled_NM), '-n', '6', str(scaled_NEA), '-n', '7',
-                         str(scaled_NWA), '-eg', '0', '7', str(rWA), '-eg', '0.000001', '6', str(rEA), '-eg',
-                         '0.000002', '4', str(rMJ), '-eg', '0.000003', '5', str(rMJ), '-em', str(scaled_TmW), '7', '3', str(scaled_mW), '-em', str(scaled_TmW+0.000001), '7', '3', '0', '-em', str(scaled_TmE), '6', '3', str(scaled_mE), '-em', str(scaled_TmE+0.000001), '6', '3', '0', '-ej', str(scaled_TAEW), '6', '7','-ej', str(scaled_TA), '7', '4', '-ej', str(scaled_TMJ), '5', '4', '-ej', str(scaled_TEM),
-                         '4', '3', '-ej', str(scaled_Teu_as), '3', '2', '-en', str(scaled_Tgrowth_Af), '1',
-                         str(scaled_NANC), '-ej', str(scaled_Taf), '2', '1']
+            macs_args = ['./bin/macs', str(total), str(length), '-t', str(macs_theta),'-s',str(seed_option), '-r', str(macs_rho), '-h', '1e5','-R', 'genetic_map_b37/genetic_map_GRCh37_chr' + str(chr_number) + '.txt.macshs', '-I', '7',str(total_naf), str(total_nas), str(total_neu), str(nJ), str(nM), str(nEA), str(nWA), '-n','1', str(scaled_NAF), '-n', '2', str(scaled_NCHB), '-n', '3', str(scaled_NCEU), '-n', '4',str(scaled_NJ), '-n', '5', str(scaled_NM), '-n', '6', str(scaled_NEA), '-n', '7',str(scaled_NWA), '-eg', '0', '7', str(rWA), '-eg', '0.000001', '6', str(rEA), '-eg','0.000002', '4', str(rMJ), '-eg', '0.000003', '5', str(rMJ), '-em', str(scaled_TmW), '7', '3', str(scaled_mW), '-em', str(scaled_TmW+0.000001), '7', '3', '0', '-em', str(scaled_TmE), '6', '3', str(scaled_mE), '-em', str(scaled_TmE+0.000001), '6', '3', '0', '-ej', str(scaled_TAEW), '6', '7','-ej', str(scaled_TA), '7', '4', '-ej', str(scaled_TMJ), '5', '4', '-ej', str(scaled_TEM),'4', '3', '-ej', str(scaled_Teu_as), '3', '2', '-en', str(scaled_Tgrowth_Af), '1',str(scaled_NANC), '-ej', str(scaled_Taf), '2', '1']
 
         if case == 4:
-            macs_args = ['./bin/macs', str(total), str(length), '-t', str(macs_theta),'-s',str(seed_option), '-r', str(macs_rho), '-h', '1e5',
-                         '-R', 'genetic_map_b37/genetic_map_GRCh37_chr' + str(chr_number) + '.txt.macshs', '-I', '7',
+            macs_args = ['./bin/macs', str(total), str(length), '-t', str(macs_theta),'-s',str(seed_option), '-r', str(macs_rho), '-h', '1e5','-R', 'genetic_map_b37/genetic_map_GRCh37_chr' + str(chr_number) + '.txt.macshs', '-I', '7',
                          str(total_naf), str(total_nas), str(total_neu), str(nJ), str(nM), str(nEA), str(nWA), '-n',
                          '1', str(scaled_NAF), '-n', '2', str(scaled_NCHB), '-n', '3', str(scaled_NCEU), '-n', '4',
                          str(scaled_NJ), '-n', '5', str(scaled_NM), '-n', '6', str(scaled_NEA), '-n', '7',
@@ -107,10 +98,7 @@ def run_sim(parameters,case,length,chr_number,total,total_naf,total_nas,total_ne
                          '1', str(scaled_NAF), '-n', '2', str(scaled_NCHB), '-n', '3', str(scaled_NCEU), '-n', '4',
                          str(scaled_NJ), '-n', '5', str(scaled_NM), '-n', '6', str(scaled_NEA), '-n', '7',
                          str(scaled_NWA), '-eg', '0', '7', str(rWA), '-eg', '0.000001', '6', str(rEA), '-eg',
-                         '0.000002', '4', str(rMJ), '-eg', '0.000003', '5', str(rMJ), '-em', str(scaled_TmW), '7', '3', str(scaled_mW), '-em', str(scaled_TmW+0.000001), '7', '3', '0', '-em', str(scaled_TmE), '6', '3', str(scaled_mE), '-em', str(scaled_TmE+0.000001), '6', '3', '0', '-ej', str(scaled_TAEW), '6', '7',
-                         '0', '-ej', str(scaled_TA), '7', '4', '-ej', str(scaled_TMJ), '5', '4', '-ej', str(scaled_TEM),
-                         '4', '3', '-en', str(scaled_Tgrowth_Af), '1', str(scaled_NANC), '-ej', str(scaled_Teu_as), '3',
-                         '2', '-ej', str(scaled_Taf), '2', '1']
+                         '0.000002', '4', str(rMJ), '-eg', '0.000003', '5', str(rMJ), '-em', str(scaled_TmW), '7', '3', str(scaled_mW), '-em', str(scaled_TmW+0.000001), '7', '3', '0', '-em', str(scaled_TmE), '6', '3', str(scaled_mE), '-em', str(scaled_TmE+0.000001), '6', '3', '0', '-ej', str(scaled_TAEW), '6', '7', '-ej', str(scaled_TA), '7', '4', '-ej', str(scaled_TMJ), '5', '4', '-ej', str(scaled_TEM),'4', '3', '-en', str(scaled_Tgrowth_Af), '1', str(scaled_NANC), '-ej', str(scaled_Teu_as), '3','2', '-ej', str(scaled_Taf), '2', '1']
 
         if case == 6:
             macs_args = ['./bin/macs', str(total), str(length), '-t', str(macs_theta),'-s',str(seed_option), '-r', str(macs_rho), '-h', '1e5',
@@ -119,8 +107,7 @@ def run_sim(parameters,case,length,chr_number,total,total_naf,total_nas,total_ne
                          '1', str(scaled_NAF), '-n', '2', str(scaled_NCHB), '-n', '3', str(scaled_NCEU), '-n', '4',
                          str(scaled_NJ), '-n', '5', str(scaled_NM), '-n', '6', str(scaled_NEA), '-n', '7',
                          str(scaled_NWA), '-eg', '0', '7', str(rWA), '-eg', '0.000001', '6', str(rEA), '-eg',
-                         '0.000002', '4', str(rMJ), '-eg', '0.000003', '5', str(rMJ), '-em', str(scaled_TmE), '6', '3', str(scaled_mE), '-em', str(scaled_TmE+0.000001), '6', '3', '0', '-em', str(scaled_TmW), '7', '3', str(scaled_mW), '-em', str(scaled_TmW+0.000001), '7', '3', '0', '-ej', str(scaled_TAEW), '6', '7',
-                         '0', '-ej', str(scaled_TA), '7', '4', '-ej', str(scaled_TMJ), '5', '4', '-ej', str(scaled_TEM),
+                         '0.000002', '4', str(rMJ), '-eg', '0.000003', '5', str(rMJ), '-em', str(scaled_TmE), '6', '3', str(scaled_mE), '-em', str(scaled_TmE+0.000001), '6', '3', '0', '-em', str(scaled_TmW), '7', '3', str(scaled_mW), '-em', str(scaled_TmW+0.000001), '7', '3', '0', '-ej', str(scaled_TAEW), '6', '7', '-ej', str(scaled_TA), '7', '4', '-ej', str(scaled_TMJ), '5', '4', '-ej', str(scaled_TEM),
                          '4', '3', '-en', str(scaled_Tgrowth_Af), '1', str(scaled_NANC), '-ej', str(scaled_Teu_as), '3',
                          '2', '-ej', str(scaled_Taf), '2', '1']
 
@@ -161,7 +148,7 @@ def run_sim(parameters,case,length,chr_number,total,total_naf,total_nas,total_ne
                          '1', str(scaled_NAF), '-n', '2', str(scaled_NCHB), '-n', '3', str(scaled_NCEU), '-n', '4',
                          str(scaled_NJ), '-n', '5', str(scaled_NM), '-n', '6', str(scaled_NEA), '-n', '7',
                          str(scaled_NWA), '-eg', '0', '7', str(rWA), '-eg', '0.000001', '6', str(rEA), '-eg',
-                         '0.000002', '4', str(rMJ), '-eg', '0.000003', '5', str(rMJ), '-em', str(scaled_TmE), '6', '3', str(scaled_mE), '-em', str(scaled_TmE+0.000001), '6', '3', '-em', str(scaled_TmW), '7', '3', str(scaled_mW), '-em', str(scaled_TmW+0.000001), '7', '3', '0', '0', '-ej', str(scaled_TAEW), '6', '7',
+                         '0.000002', '4', str(rMJ), '-eg', '0.000003', '5', str(rMJ), '-em', str(scaled_TmE), '6', '3', str(scaled_mE), '-em', str(scaled_TmE+0.000001), '6', '3', '0','-em', str(scaled_TmW), '7', '3', str(scaled_mW), '-em', str(scaled_TmW+0.000001), '7', '3', '0', '-ej', str(scaled_TAEW), '6', '7',
                          '-ej', str(scaled_TA), '7', '4', '-en', str(scaled_Tgrowth_Af), '1', str(scaled_NANC),
                          '-ej', str(scaled_TMJ), '5', '4', '-ej', str(scaled_TEM), '4', '3', '-ej', str(scaled_Teu_as),
                          '3', '2', '-ej', str(scaled_Taf), '2', '1']
@@ -259,8 +246,7 @@ def run_sim(parameters,case,length,chr_number,total,total_naf,total_nas,total_ne
                          '1', str(scaled_NAF), '-n', '2', str(scaled_NCHB), '-n', '3', str(scaled_NCEU), '-n', '4',
                          str(scaled_NJ), '-n', '5', str(scaled_NM), '-n', '6', str(scaled_NEA), '-n', '7',
                          str(scaled_NWA), '-eg', '0', '7', str(rWA), '-eg', '0.000001', '6', str(rEA), '-eg',
-                         '0.000002', '4', str(rMJ), '-eg', '0.000003', '5', str(rMJ), '-em', str(scaled_TmW), '7', '3', str(scaled_mW), '-em', str(scaled_TmW+0.000001), '7', '3', '0', '-em', str(scaled_TmE), '6', '3', str(scaled_mE), '-em', str(scaled_TmE+0.000001), '6', '3', '0', '-ej', str(scaled_TAEW), '6', '7',
-                         '0', '-ej', str(scaled_TA), '7', '4', '-ej', str(scaled_TMJ), '5', '4', '-ej', str(scaled_TEM),
+                         '0.000002', '4', str(rMJ), '-eg', '0.000003', '5', str(rMJ), '-em', str(scaled_TmW), '7', '3', str(scaled_mW), '-em', str(scaled_TmW+0.000001), '7', '3', '0', '-em', str(scaled_TmE), '6', '3', str(scaled_mE), '-em', str(scaled_TmE+0.000001), '6', '3', '0', '-ej', str(scaled_TAEW), '6', '7', '-ej', str(scaled_TA), '7', '4', '-ej', str(scaled_TMJ), '5', '4', '-ej', str(scaled_TEM),
                          '4', '3', '-ej', str(scaled_Teu_as), '3', '2', '-ej', str(scaled_Taf), '2', '1', '-en',
                          str(scaled_Tgrowth_Af), '1', str(scaled_NANC)]
 
@@ -304,8 +290,7 @@ def run_sim(parameters,case,length,chr_number,total,total_naf,total_nas,total_ne
                          '1', str(scaled_NAF), '-n', '2', str(scaled_NCHB), '-n', '3', str(scaled_NCEU), '-n', '4',
                          str(scaled_NJ), '-n', '5', str(scaled_NM), '-n', '6', str(scaled_NEA), '-n', '7',
                          str(scaled_NWA), '-eg', '0', '7', str(rWA), '-eg', '0.000001', '6', str(rEA), '-eg',
-                         '0.000002', '4', str(rMJ), '-eg', '0.000003', '5', str(rMJ), '-em', str(scaled_TmW), '7', '3', str(scaled_mW), '-em', str(scaled_TmW+0.000001), '7', '3', '0', '-em', str(scaled_TmE), '6', '3', str(scaled_mE), '-em', str(scaled_TmE+0.000001), '6', '3', '0', '-ej', str(scaled_TAEW), '6', '7',
-                         '0', '-ej', str(scaled_TA), '7', '4', '-ej', str(scaled_TMJ), '5', '4', '-ej', str(scaled_TEM),
+                         '0.000002', '4', str(rMJ), '-eg', '0.000003', '5', str(rMJ), '-em', str(scaled_TmW), '7', '3', str(scaled_mW), '-em', str(scaled_TmW+0.000001), '7', '3', '0', '-em', str(scaled_TmE), '6', '3', str(scaled_mE), '-em', str(scaled_TmE+0.000001), '6', '3', '0', '-ej', str(scaled_TAEW), '6', '7', '-ej', str(scaled_TA), '7', '4', '-ej', str(scaled_TMJ), '5', '4', '-ej', str(scaled_TEM),
                          '4', '3', '-en', str(scaled_Tgrowth_Af), '1', str(scaled_NANC), '-ej', str(scaled_Teu_as), '3',
                          '2', '-ej', str(scaled_Taf), '2', '1']
 
@@ -316,8 +301,7 @@ def run_sim(parameters,case,length,chr_number,total,total_naf,total_nas,total_ne
                          '1', str(scaled_NAF), '-n', '2', str(scaled_NCHB), '-n', '3', str(scaled_NCEU), '-n', '4',
                          str(scaled_NJ), '-n', '5', str(scaled_NM), '-n', '6', str(scaled_NEA), '-n', '7',
                          str(scaled_NWA), '-eg', '0', '7', str(rWA), '-eg', '0.000001', '6', str(rEA), '-eg',
-                         '0.000002', '4', str(rMJ), '-eg', '0.000003', '5', str(rMJ), '-em', str(scaled_TmE), '6', '3', str(scaled_mE), '-em', str(scaled_TmE+0.000001), '6', '3', '0', '-em', str(scaled_TmW), '7', '3', str(scaled_mW), '-em', str(scaled_TmW+0.000001), '7', '3', '0', '-ej', str(scaled_TAEW), '6', '7',
-                         '0', '-ej', str(scaled_TA), '7', '4', '-ej', str(scaled_TMJ), '5', '4', '-ej', str(scaled_TEM),
+                         '0.000002', '4', str(rMJ), '-eg', '0.000003', '5', str(rMJ), '-em', str(scaled_TmE), '6', '3', str(scaled_mE), '-em', str(scaled_TmE+0.000001), '6', '3', '0', '-em', str(scaled_TmW), '7', '3', str(scaled_mW), '-em', str(scaled_TmW+0.000001), '7', '3', '0', '-ej', str(scaled_TAEW), '6', '7', '-ej', str(scaled_TA), '7', '4', '-ej', str(scaled_TMJ), '5', '4', '-ej', str(scaled_TEM),
                          '4', '3', '-en', str(scaled_Tgrowth_Af), '1', str(scaled_NANC), '-ej', str(scaled_Teu_as), '3',
                          '2', '-ej', str(scaled_Taf), '2', '1']
 
@@ -358,7 +342,7 @@ def run_sim(parameters,case,length,chr_number,total,total_naf,total_nas,total_ne
                          '1', str(scaled_NAF), '-n', '2', str(scaled_NCHB), '-n', '3', str(scaled_NCEU), '-n', '4',
                          str(scaled_NJ), '-n', '5', str(scaled_NM), '-n', '6', str(scaled_NEA), '-n', '7',
                          str(scaled_NWA), '-eg', '0', '7', str(rWA), '-eg', '0.000001', '6', str(rEA), '-eg',
-                         '0.000002', '4', str(rMJ), '-eg', '0.000003', '5', str(rMJ), '-em', str(scaled_TmE), '6', '3', str(scaled_mE), '-em', str(scaled_TmE+0.000001), '6', '3', '-em', str(scaled_TmW), '7', '3', str(scaled_mW), '-em', str(scaled_TmW+0.000001), '7', '3', '0', '0', '-ej', str(scaled_TAEW), '6', '7',
+                         '0.000002', '4', str(rMJ), '-eg', '0.000003', '5', str(rMJ), '-em', str(scaled_TmE), '6', '3', str(scaled_mE), '-em', str(scaled_TmE+0.000001), '6', '3','0', '-em', str(scaled_TmW), '7', '3', str(scaled_mW), '-em', str(scaled_TmW+0.000001), '7', '3', '0', '-ej', str(scaled_TAEW), '6', '7',
                          '-ej', str(scaled_TA), '7', '4', '-en', str(scaled_Tgrowth_Af), '1', str(scaled_NANC),
                          '-ej', str(scaled_TMJ), '5', '4', '-ej', str(scaled_TEM), '4', '3', '-ej', str(scaled_Teu_as),
                          '3', '2', '-ej', str(scaled_Taf), '2', '1']
