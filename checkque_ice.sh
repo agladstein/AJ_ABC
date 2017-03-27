4929f6cbb4c997996ec3c4aad6d30357fdfbddca
Ariella Gladstein <agladstein@email.arizona.edu>
2017-03-27 12:08:35 -0700

    add model choice and submiting qualified jobs

--- checkque_ice.sh
+++ checkque_ice.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
-cd /rsgrps/mfh/agladstein/Simulations/macsSwig_AJmodels
+cd /home/u15/agladstein/ABC/macsSwig_AJmodels
 
 GOAL=$1
-RESULTS=$2
-echo "Check for ${GOAL} completed runs in $RESULTS"
-QUEMAX=$3
-#PBS=$4
-SYSTEM=$4 #smp, clu, htc
+QUEMAX=$2
+OUT=$3
+MODEL=$4
+SYSTEM=$5 #smp, clu, htc
 qstat=/usr/local/bin/qstat_local
 qsub=/usr/pbs/bin/qsub
+
+RESULTS=${OUT}/results_sims_AJ_M${MODEL}
+echo "Check for ${GOAL} completed runs in $RESULTS"
 
 #check number of completed simulations
 COMP=$(ls $RESULTS/ | wc -l)
@@ -30,9 +32,16 @@ else
 	    echo "That's enough jobs in the que"
 
     else
+	    #create PBS scripts
+	    if [ "$SYSTEM" == "clu" ]; then
+            ./main_function_AJmodel_j2.sh cluster /rsgrps/mfh4/Ariella/macsSwig_AJmodels ${MODEL}
+        else
+            ./main_function_AJmodel_j2.sh ${SYSTEM} /rsgrps/mfh4/Ariella/macsSwig_AJmodels ${MODEL}
+        fi
+
 	    #check hrs left in group
 	    QHRS=$(va | cut -f3 | tail -1 | cut -d ":" -f1)
-	    QBOUND=20000 #0
+	    QBOUND=0
 	    SHRS=$(va | cut -f2 | tail -1 | cut -d ":" -f1)
 	    DAYS=$(( $(($(cal | wc -w) - 9)) - $(date | cut -d " " -f3) ))
 	    SBOUND=$(( $DAYS * 350))
@@ -40,8 +49,10 @@ else
 	    echo "${QHRS} mfh qualified hrs are left"
 	    if [ "$QHRS" -gt "$QBOUND" ]; then
 	        echo "Submit to qualified"
+	        echo "qsub model${MODEL}_${SYSTEM}_qualified.pbs"
 	    else
 	        echo "There are no qualified hrs left to use"
+
 	        echo "Try standard"
 	        echo "${SHRS} mfh standard hrs are left"
 	        echo "There are $DAYS days left in the month"
--- main_function_AJmodel_j2.sh
+++ main_function_AJmodel_j2.sh
@@ -1,33 +1,26 @@
 #!/usr/bin/env bash
 SYSTEM=$1
 OUT=$2
+MODEL=$3
 
 if [ "$SYSTEM" == "ocelote" ]; then
     VIRTUAL_ENV=/home/u15/agladstein/env/macss_env_ocelote_2.7.11
     PATH=${VIRTUAL_ENV}/bin:$PATH
-    for i in {1..3}
-    do
-	    QUE=standard JNUM=1-2500 NODE=1 CORE=1 MEM=6 PYTHONENV=${VIRTUAL_ENV}/bin/python MODEL=${i} OUT_PATH=${OUT} j2 template.pbs.j2 >model${i}_${SYSTEM}_standard.pbs
-	    QUE=qualified JNUM=1-2500 NODE=1 CORE=1 MEM=6 PYTHONENV=${VIRTUAL_ENV}/bin/python MODEL=${i} OUT_PATH=${OUT} j2 template.pbs.j2 >model${i}_${SYSTEM}_qualified.pbs
-	    QUE=windfall JNUM=1-5000 NODE=1 CORE=1 MEM=6 PYTHONENV=${VIRTUAL_ENV}/bin/python MODEL=${i} OUT_PATH=${OUT} j2 template.pbs.j2 >model${i}_${SYSTEM}_windfall.pbs
-    done
+	QUE=standard JNUM=1-2500 NODE=1 CORE=1 MEM=6 PYTHONENV=${VIRTUAL_ENV}/bin/python MODEL=${MODEL} OUT_PATH=${OUT} j2 template.pbs.j2 >model${MODEL}_${SYSTEM}_standard.pbs
+	QUE=qualified JNUM=1-2500 NODE=1 CORE=1 MEM=6 PYTHONENV=${VIRTUAL_ENV}/bin/python MODEL=${MODEL} OUT_PATH=${OUT} j2 template.pbs.j2 >model${MODEL}_${SYSTEM}_qualified.pbs
+	QUE=windfall JNUM=1-5000 NODE=1 CORE=1 MEM=6 PYTHONENV=${VIRTUAL_ENV}/bin/python MODEL=${MODEL} OUT_PATH=${OUT} j2 template.pbs.j2 >model${MODEL}_${SYSTEM}_windfall.pbs
+
 else
     VIRTUAL_ENV=/home/u15/agladstein/env/macss_env_ICE_2.7.9
     PATH=${VIRTUAL_ENV}/bin:$PATH
     if [ "$SYSTEM" == "smp" ]; then
         ICE_MEM=8
-        for i in {1..3}
-        do
-	        QUE=${SYSTEM}_qual JNUM=1-1500 NODE=1 CORE=1 MEM=${ICE_MEM} PYTHONENV=${VIRTUAL_ENV}/bin/python MODEL=${i} JOBTYPE=${SYSTEM}_only OUT_PATH=${OUT} j2 template.pbs.j2 >model${i}_${SYSTEM}_qualified.pbs
-        done
+	    QUE=${SYSTEM}_qual JNUM=1-1500 NODE=1 CORE=1 MEM=${ICE_MEM} PYTHONENV=${VIRTUAL_ENV}/bin/python MODEL=${MODEL} JOBTYPE=${SYSTEM}_only OUT_PATH=${OUT} j2 template.pbs.j2 >model${MODEL}_${SYSTEM}_qualified.pbs
     else
         ICE_MEM=4
     fi
-    for i in {1..3}
-    do
-        QUE=standard JNUM=1-1500 NODE=1 CORE=1 MEM=${ICE_MEM} PYTHONENV=${VIRTUAL_ENV}/bin/python MODEL=${i} JOBTYPE=${SYSTEM}_only OUT_PATH=${OUT} j2 template.pbs.j2 >model${i}_${SYSTEM}_standard.pbs
-	    QUE=windfall JNUM=1-5000 NODE=1 CORE=1 MEM=${ICE_MEM} PYTHONENV=${VIRTUAL_ENV}/bin/python MODEL=${i} JOBTYPE=${SYSTEM}_only OUT_PATH=${OUT} j2 template.pbs.j2 >model${i}_${SYSTEM}_windfall.pbs
-    done
+    QUE=standard JNUM=1-1500 NODE=1 CORE=1 MEM=${ICE_MEM} PYTHONENV=${VIRTUAL_ENV}/bin/python MODEL=${MODEL} JOBTYPE=${SYSTEM}_only OUT_PATH=${OUT} j2 template.pbs.j2 >model${MODEL}_${SYSTEM}_standard.pbs
+	QUE=windfall JNUM=1-5000 NODE=1 CORE=1 MEM=${ICE_MEM} PYTHONENV=${VIRTUAL_ENV}/bin/python MODEL=${MODEL} JOBTYPE=${SYSTEM}_only OUT_PATH=${OUT} j2 template.pbs.j2 >model${MODEL}_${SYSTEM}_windfall.pbs
 fi
 
 
