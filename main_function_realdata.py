119c3b444a64b9e1b38873f726b1879c95ef8012
Ariella Gladstein <agladstein@email.arizona.edu>
2017-03-29 12:43:41 -0700

    corrected duplicate IBD_num_EE in header

--- main_function_realdata.py
+++ main_function_realdata.py
@@ -187,7 +187,7 @@ def main():
         res.extend(IBDlengths_median30)
         head = head + 'IBD30_median_AA\tIBD30_median_JJ\tIBD30_median_MM\tIBD30_median_EE\tIBD30_median_AE\tIBD30_median_AJ\tIBD30_median_AM\tIBD30_median_JM\tIBD30_median_JE\tIBD30_median_ME\t'
         res.extend(IBDlengths_num30)
-        head = head + 'IBD30_num_AA\tIBD30_num_JJ\tIBD30_num_MM\tIBD_num_EE\tIBD30_num_AE\tIBD30_num_AJ\tIBD30_num_AM\tIBD30_num_JM\tIBD30_num_JE\tIBD30_num_ME\t'
+        head = head + 'IBD30_num_AA\tIBD30_num_JJ\tIBD30_num_MM\tIBD30_num_EE\tIBD30_num_AE\tIBD30_num_AJ\tIBD30_num_AM\tIBD30_num_JM\tIBD30_num_JE\tIBD30_num_ME\t'
         res.extend(IBDlengths_var30)
         head = head + 'IBD30_var_AA\tIBD30_var_JJ\tIBD30_var_MM\tIBD_var_EE\tIBD30_var_AE\tIBD30_var_AJ\tIBD30_var_AM\tIBD30_var_JM\tIBD30_var_JE\tIBD30_var_ME\t'
 
