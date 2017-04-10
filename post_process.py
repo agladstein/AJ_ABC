da9d82fe14e7aade0dd0155ea782465ff34bbd6e
Ariella Gladstein <agladstein@email.arizona.edu>
2017-04-10 16:50:44 -0700

    multiporcessing to combine results and sim_values into one file with header input file.

--- post_process.py
+++ post_process.py
@@ -1,26 +1,29 @@
+from sys import stderr
 from sys import argv
 import os.path
 import linecache
 import multiprocessing
-import glob
+import functools
+from os import listdir
 
-def combine_files(f, fileout, n, sim_path_results, sim_path_values):
-    print str(sim_path_results) + "/" + str(f)
-    if len(f.split("_")) == 4:
-        job_id = str(f.split(".")[0].split("_")[2]) + "_" + str(f.split(".")[0].split("_")[3])
-    elif len(f.split("_")) == 3:
-        job_id = f.split("_")[2].split(".")[0]
-    sim_values_file_name = str(sim_path_values) + "/sim_" + str(job_id) + "_values.txt"
-    if os.path.exists(sim_values_file_name):
-        line = str(job_id) + "\t" + linecache.getline(sim_values_file_name, 2).rstrip('\n') + "\t" + linecache.getline(
-            str(sim_path_results) + "/" + str(f), 2)
-        if len(line.split('\t')) == n:
-            print sim_values_file_name
-            fileout.write(
-                str(job_id) + "\t" + linecache.getline(sim_values_file_name, 2).rstrip('\n') + "\t" + linecache.getline(
-                    str(sim_path_results) + "/" + str(f), 2))
-    else:
-        print str(sim_values_file_name) + " does not exist"
+def combine_files(n, sim_path_results, sim_path_values, f):
+    if f.endswith(".summary"):
+        if len(f.split("_")) == 4:
+            job_id = str(f.split(".")[0].split("_")[2]) + "_" + str(f.split(".")[0].split("_")[3])
+        elif len(f.split("_")) == 3:
+            job_id = f.split("_")[2].split(".")[0]
+        sim_values_file_name = str(sim_path_values) + "/sim_" + str(job_id) + "_values.txt"
+        if os.path.exists(sim_values_file_name):
+            line = str(job_id) + "\t" + linecache.getline(sim_values_file_name, 2).rstrip('\n') + "\t" + linecache.getline(str(sim_path_results) + "/" + str(f), 2)
+            if len(line.split('\t')) == n:
+                print str(sim_path_results) + "/" + str(f)
+                print sim_values_file_name
+                combined_string = str(job_id) + "\t" + linecache.getline(sim_values_file_name, 2).rstrip('\n') + "\t" + linecache.getline(str(sim_path_results) + "/" + str(f), 2)
+                return combined_string
+            else:
+                stderr.write(str(sim_path_results) + " does not have the desired number of summary statistics\n")
+        else:
+            stderr.write(str(sim_values_file_name) + " does not exist\n")
 
 
 def main():
@@ -40,16 +43,16 @@ def main():
     sim_path_results = str(sim_path)+"/results_sims_AJ_M"+str(model)
     sim_path_values = str(sim_path)+"/sim_values_AJ_M"+str(model)
 
-    # Find all .summary files in results directory
-    files_results = glob.glob('{}/results_sims_AJ_M{}/*.summary'.format(sim_path, model))
+    files_results = listdir(sim_path_results)
+
+    function_to_map = functools.partial(combine_files, n, sim_path_results, sim_path_values)
+    results = pool.imap_unordered(function_to_map, files_results, 10)
 
     out_file_name = str(out_path)+"/input_ABCtoolbox_M"+str(model)+"_"+str(len(files_results))+".txt"
     fileout = open(out_file_name, 'w')
     fileout.write(head)
-
-
-    results = pool.imap_unordered(combine_files(files_results, fileout, n, sim_path_results, sim_path_values), files_results, 10)
-
+    for result in results:
+        fileout.write(result)
 
 
 if __name__ == '__main__':
