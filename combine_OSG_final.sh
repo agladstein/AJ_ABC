7359241e335869be0d21cbab8b6c5ba9bde1fbb8
Ariella Gladstein <agladstein@email.arizona.edu>
2017-04-29 10:23:08 -0700

    combine OSG results and sim values for ABC input

--- combine_OSG_final.sh
+++ combine_OSG_final.sh
@@ -6,19 +6,11 @@ do
         echo $workflow
         MODEL=$(head -2 $workflow/sim_values.txt | tail -1 | cut -d '/' -f1 | cut -d '_' -f4)
         echo ${MODEL}
-        if [ -e sim_values_OSG_${MODEL}.txt ] ; then
-            echo 'grep -v "Asc_NAF" $workflow/sim_values.txt >>sim_values_OSG_${MODEL}.txt'
-            grep -v "Asc_NAF" $workflow/sim_values.txt >>sim_values_OSG_${MODEL}.txt
+
+        if [ -e input_ABC_OSG_${MODEL}.txt ] ; then
+            paste $workflow/sim_values.txt $workflow/results_sims.txt | grep -v "Asc_NAF" >>input_ABC_OSG_${MODEL}.txt
         else
-            echo 'cat $workflow/sim_values.txt >sim_values_OSG_${MODEL}.txt'
-            cat $workflow/sim_values.txt >sim_values_OSG_${MODEL}.txt
-        fi
-        if [ -e results_sims_OSG_${MODEL}.txt ] ; then
-            echo 'grep -v "SegS_Af_CGI" $workflow/results_sims.txt >>results_sims_OSG_${MODEL}.txt'
-            grep -v "SegS_Af_CGI" $workflow/results_sims.txt >>results_sims_OSG_${MODEL}.txt
-        else
-            echo 'cat $workflow/results_sims.txt >results_sims_OSG_${MODEL}.txt'
-            cat $workflow/results_sims.txt >results_sims_OSG_${MODEL}.txt
+            paste $workflow/sim_values.txt $workflow/results_sims.txt >input_ABC_OSG_${MODEL}.txt
         fi
     fi
 done
