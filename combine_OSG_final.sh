7bfb25ba884e2eaa0d7e4010a8d86cebc34e1d82
Ariella Gladstein <agladstein@email.arizona.edu>
2017-04-29 10:33:36 -0700

    make different ABC input files for model 1 with only A vs wA and eA

--- combine_OSG_final.sh
+++ combine_OSG_final.sh
@@ -7,10 +7,20 @@ do
         MODEL=$(head -2 $workflow/sim_values.txt | tail -1 | cut -d '/' -f1 | cut -d '_' -f4)
         echo ${MODEL}
 
-        if [ -e input_ABC_OSG_${MODEL}.txt ] ; then
-            paste $workflow/sim_values.txt $workflow/results_sims.txt | grep -v "Asc_NAF" >>input_ABC_OSG_${MODEL}.txt
+        if [ ${MODEL} == "M1" ] ; then
+            NSTATS=$(head -1 $workflow/results_sims.txt | tr '\t' '\n' | wc -l)
+            if [ -e input_ABC_OSG_${MODEL}_${NSTATS}.txt ] ; then
+                paste $workflow/sim_values.txt $workflow/results_sims.txt | grep -v "Asc_NAF" >>input_ABC_OSG_${MODEL}_${NSTATS}.txt
+            else
+                paste $workflow/sim_values.txt $workflow/results_sims.txt >input_ABC_OSG_${MODEL}_${NSTATS}.txt
+            fi
+
         else
-            paste $workflow/sim_values.txt $workflow/results_sims.txt >input_ABC_OSG_${MODEL}.txt
+            if [ -e input_ABC_OSG_${MODEL}.txt ] ; then
+                paste $workflow/sim_values.txt $workflow/results_sims.txt | grep -v "Asc_NAF" >>input_ABC_OSG_${MODEL}.txt
+            else
+                paste $workflow/sim_values.txt $workflow/results_sims.txt >input_ABC_OSG_${MODEL}.txt
+            fi
         fi
     fi
 done
