53d01adce5a8427f2e0917d39f844cba636d9cc6
Ariella Gladstein <agladstein@email.arizona.edu>
2017-04-29 18:48:01 -0700

    begin to refactor with Pandas

--- subset_stats.py
+++ subset_stats.py
@@ -1,6 +1,6 @@
 from sys import argv
-import csv
-
+import pandas as pd
+import itertools
 
 def get_corr_stats(stats_file):
     '''This option should be used to remove highly correlated statistics from ABCtoolbox input.
@@ -15,70 +15,37 @@ def get_corr_stats(stats_file):
                 stats.append(columns[1])
     return stats
 
-def get_power_stats(stats_file):
+def get_power_stats(stats_file, n_sets):
     ''' This option should be used to only keep parameter values and statistics with high power from ABCtoolbox input. 
     stats_file should be the output file *greedySearchForBestStatisticsForModelChoice.txt after running ABCestimator with the option findStatsModelChoice'''
 
     print 'Reading summary statistics found in ' + stats_file + '\n'
-    # with open(stats_file) as keep_file:
-    #     for line in keep_file:
-    #
-    # return stats
-
-def remove_stats(stats, input_file):
-    print 'Pruning correlated summary statistics \n'
-    with open(input_file) as prune_input_file:
-        reader = csv.reader(prune_input_file, delimiter='\t')
-        head = reader.next()
-    rm_index = []
-    for i, item in enumerate(head):
-        if item in stats:
-            continue
-        else:
-            rm_index.append(i)
-    return rm_index
-
-
-def keep_stats(stats, input_file):
-    print 'Keeping higher power summary statistics \n'
-
-    # return out_string
-
-
-def get_string(input_file, rm_index):
-    out_string = ''
-    with open(input_file) as prune_input_file:
-        for line in prune_input_file:
-            line = line.strip('\n')
-            columns = line.split('\t')
-            for j in rm_index:
-                out_string = out_string + columns[j] + '\t'
-            out_string = out_string + '\n'
-    return out_string
+    keep_file_df = pd.read_csv(stats_file, sep = '\t')
+    stats = list(itertools.chain(*keep_file_df.head(n_sets)['Statistics(Names)'].str.split(',').tolist()))
+    return stats
 
 
 def main():
     stats_file = argv[1]
     input_file = argv[2]
     option = argv[3] # remove or keep
+    n_sets = argv[4] # number of sets of statistics from model choice power analysis
+
+    input_file_df = pd.read_csv(input_file, sep='\t')
 
     if option == "remove":
         stats = get_corr_stats(stats_file)
-        index = remove_stats(stats, input_file)
         out_file_name = 'pruneCorStats_'+input_file
     elif option == "keep":
-        stats = get_power_stats(stats_file)
-        index = keep_stats(stats, input_file)
+        if n_sets < 1:
+            print 'Tell me how many sets of statistics to use!'
+            return
+        stats = get_power_stats(stats_file, n_sets)
         out_file_name = 'keepPowerStats_' + input_file
+        input_file_df[stats].to_csv(out_file_name, sep='\t')
     else:
         print "You must specify remove or keep!"
         return
-
-    out_string = get_string(input_file, index)
-
-    fileout = open(out_file_name, 'w')
-    fileout.write(out_string)
-    fileout.close()
 
 if __name__ == '__main__':
     main()
