8c9ee3128dca95398ff7a3d271aef6923bb5a3f7
Ariella Gladstein <agladstein@email.arizona.edu>
2017-05-01 12:58:18 -0700

    simplified arguements. used index=False to correctly create file

--- subset_stats/main_subset_sim.py
+++ subset_stats/main_subset_sim.py
@@ -9,12 +9,8 @@ def main():
     input_file = argv[2]
     option = argv[3]  # remove or keep
     n_sets = int(argv[4])  # number of sets of statistics from model choice power analysis
-    if len(argv) == 5:
-        sim_id = 'sim'
-    else:
-        sim_id = argv[5]  # the header for sim id
 
-    input_file_df = pd.read_csv(input_file, sep="\t", index_col=sim_id)
+    input_file_df = pd.read_csv(input_file, sep="\t")
     params_df = get_params(input_file_df)
 
     if option == "remove":
@@ -29,7 +25,7 @@ def main():
         stats = get_power_stats(stats_file, n_sets)
         out_file_name = 'keepPowerStats_' + input_file
         print 'Creating new file with parameters and summary statistics with high power'
-        pd.concat([params_df, input_file_df[stats]], axis=1, join='inner').to_csv(out_file_name, sep='\t')
+        pd.concat([params_df, input_file_df[stats]], axis=1, join='inner').to_csv(out_file_name, sep='\t', index=False)
     else:
         print "You must specify remove or keep!"
         return
