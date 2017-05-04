0b72e8655852db85f9ee4c2cc55f71001935f2ef
Ariella Gladstein <agladstein@email.arizona.edu>
2017-05-04 10:17:59 -0700

    include load module in template

--- main_function_AJmodel_j2.sh
+++ main_function_AJmodel_j2.sh
@@ -4,15 +4,19 @@ OUT=$2
 MODEL=$3
 
 if [ "$SYSTEM" == "ocelote" ]; then
-    module load python/2/2.7.11
-    VIRTUAL_ENV=/home/u15/agladstein/env/macss_env_ocelote_2.7.11
-    PATH=${VIRTUAL_ENV}/bin:$PATH
-	QUE=standard JNUM=1-2500 NODE=1 CORE=1 MEM=6 PYTHONENV=${VIRTUAL_ENV}/bin/python MODEL=${MODEL} OUT_PATH=${OUT} j2 template.pbs.j2 >model${MODEL}_${SYSTEM}_standard.pbs
-	QUE=qualified JNUM=1-2500 NODE=1 CORE=1 MEM=6 PYTHONENV=${VIRTUAL_ENV}/bin/python MODEL=${MODEL} OUT_PATH=${OUT} j2 template.pbs.j2 >model${MODEL}_${SYSTEM}_qualified.pbs
-	QUE=windfall JNUM=1-5000 NODE=1 CORE=1 MEM=6 PYTHONENV=${VIRTUAL_ENV}/bin/python MODEL=${MODEL} OUT_PATH=${OUT} j2 template.pbs.j2 >model${MODEL}_${SYSTEM}_windfall.pbs
-
+#    PYTHONMOD=python/2/2.7.11
+#    module load ${PYTHONMOD}
+#    PBS_ENV=/home/u15/agladstein/env/macss_env_ocelote_2.7.11
+#    module load python/3/3.5.2
+#    VIRTUAL_ENV=/12kx/gsfs2/home/u15/agladstein/ABC/macsSwig_AJmodels/j2_env_ocelote
+#    PATH=${VIRTUAL_ENV}/bin:$PATH
+#	PYTHONMOD=${PYTHONMOD} QUE=standard JNUM=1-2500 NODE=1 CORE=1 MEM=6 PYTHONENV=${PBS_ENV}/bin/python MODEL=${MODEL} OUT_PATH=${OUT} j2 template.pbs.j2 >model${MODEL}_${SYSTEM}_standard.pbs
+#	PYTHONMOD=${PYTHONMOD} QUE=qualified JNUM=1-2500 NODE=1 CORE=1 MEM=6 PYTHONENV=${PBS_ENV}/bin/python MODEL=${MODEL} OUT_PATH=${OUT} j2 template.pbs.j2 >model${MODEL}_${SYSTEM}_qualified.pbs
+#	PYTHONMOD=${PYTHONMOD} QUE=windfall JNUM=1-5000 NODE=1 CORE=1 MEM=6 PYTHONENV=${PBS_ENV}/bin/python MODEL=${MODEL} OUT_PATH=${OUT} j2 template.pbs.j2 >model${MODEL}_${SYSTEM}_windfall.pbs
+    echo "skipping jinja, pbs already made"
 else
-    module load python/2.7.9
+    PYTHONMOD=python/2.7.9
+    module load ${PYTHONMOD}
     VIRTUAL_ENV=/home/u15/agladstein/env/macss_env_ICE_2.7.9
     PATH=${VIRTUAL_ENV}/bin:$PATH
     if [ "$SYSTEM" == "smp" ]; then
@@ -21,8 +25,8 @@ else
     else
         ICE_MEM=4
     fi
-    QUE=standard JNUM=1-1500 NODE=1 CORE=1 MEM=${ICE_MEM} PYTHONENV=${VIRTUAL_ENV}/bin/python MODEL=${MODEL} JOBTYPE=${SYSTEM}_only OUT_PATH=${OUT} j2 template.pbs.j2 >model${MODEL}_${SYSTEM}_standard.pbs
-	QUE=windfall JNUM=1-5000 NODE=1 CORE=1 MEM=${ICE_MEM} PYTHONENV=${VIRTUAL_ENV}/bin/python MODEL=${MODEL} JOBTYPE=${SYSTEM}_only OUT_PATH=${OUT} j2 template.pbs.j2 >model${MODEL}_${SYSTEM}_windfall.pbs
+    PYTHONMOD=${PYTHONMOD} QUE=standard JNUM=1-1500 NODE=1 CORE=1 MEM=${ICE_MEM} PYTHONENV=${VIRTUAL_ENV}/bin/python MODEL=${MODEL} JOBTYPE=${SYSTEM}_only OUT_PATH=${OUT} j2 template.pbs.j2 >model${MODEL}_${SYSTEM}_standard.pbs
+	PYTHONMOD=${PYTHONMOD} QUE=windfall JNUM=1-5000 NODE=1 CORE=1 MEM=${ICE_MEM} PYTHONENV=${VIRTUAL_ENV}/bin/python MODEL=${MODEL} JOBTYPE=${SYSTEM}_only OUT_PATH=${OUT} j2 template.pbs.j2 >model${MODEL}_${SYSTEM}_windfall.pbs
 fi
 
 
