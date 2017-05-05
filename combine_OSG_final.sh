b90c2c47582770e595eb2f331245ab6e8734fce3
Ariella Gladstein <agladstein@email.arizona.edu>
2017-05-04 17:22:27 -0700

    combine sim_values and results_sims without id from results_sims

--- combine_OSG_final.sh
+++ combine_OSG_final.sh
@@ -7,20 +7,23 @@ do
         MODEL=$(head -2 $workflow/sim_values.txt | tail -1 | cut -d '/' -f1 | cut -d '_' -f4)
         echo ${MODEL}
 
+        cut -f2- $workflow/results_sims.txt > $workflow/cut_results_sims.txt
+
         if [ ${MODEL} == "M1" ] ; then
             NSTATS=$(head -1 $workflow/results_sims.txt | tr '\t' '\n' | wc -l)
             if [ -e input_ABC_OSG_${MODEL}_${NSTATS}.txt ] ; then
-                paste $workflow/sim_values.txt $workflow/results_sims.txt | grep -v "Asc_NAF" >>input_ABC_OSG_${MODEL}_${NSTATS}.txt
+                paste $workflow/sim_values.txt $workflow/cut_results_sims.txt | grep -v "Asc_NAF" >>input_ABC_OSG_${MODEL}_${NSTATS}.txt
             else
-                paste $workflow/sim_values.txt $workflow/results_sims.txt >input_ABC_OSG_${MODEL}_${NSTATS}.txt
+                paste $workflow/sim_values.txt $workflow/cut_results_sims.txt >input_ABC_OSG_${MODEL}_${NSTATS}.txt
             fi
 
         else
             if [ -e input_ABC_OSG_${MODEL}.txt ] ; then
-                paste $workflow/sim_values.txt $workflow/results_sims.txt | grep -v "Asc_NAF" >>input_ABC_OSG_${MODEL}.txt
+                paste $workflow/sim_values.txt $workflow/cut_results_sims.txt | grep -v "Asc_NAF" >>input_ABC_OSG_${MODEL}.txt
             else
-                paste $workflow/sim_values.txt $workflow/results_sims.txt >input_ABC_OSG_${MODEL}.txt
+                paste $workflow/sim_values.txt $workflow/cut_results_sims.txt >input_ABC_OSG_${MODEL}.txt
             fi
         fi
+        rm $workflow/cut_results_sims.txt
     fi
 done
