6aec531802278d159cb80455826e547d93f8f70e
Ariella Gladstein <agladstein@email.arizona.edu>
2017-05-10 17:35:40 -0700

    correctly check if directory exists in google drive and use first part of PBS job id

--- drive_rsync_rm.sh
+++ drive_rsync_rm.sh
@@ -3,6 +3,8 @@
 set -e # quits at first error
 set -x # print more debugging in stdout
 
+module load irods
+
 MODEL=$1
 OUT_PATH=$2
 SIM=$3
@@ -11,57 +13,69 @@ PBS_ID=$5
 
 IP_ADDRESS=$(curl https://gist.githubusercontent.com/agladstein/2bdc122f50314f2a4c7cbc9544e7a325/raw/8bfef8b8f3f7c43fd99832a323ef7130f98571bb/atmo_instance_ip.txt)
 
+if [ ! -f ${OUT_PATH}/results_sims_AJ_M${MODEL}/${RESULTS} ] && [ ! -f ${OUT_PATH}/sim_values_AJ_M${MODEL}/${SIM} ]; then
+    echo ${OUT_PATH}/results_sims_AJ_M${MODEL}/${RESULTS} 'and' ${OUT_PATH}/sim_values_AJ_M${MODEL}/${SIM} 'not found'
+    exit 1
+fi
 
 # backup to google drive
-echo 'google driving ' ${SIM}
-if [[ $(~/bin/drive ls backup_macsSwig_AJmodels_mfloat/sim_values_AJ_M${MODEL}/$PBS_ID | grep -q "cannot be found remotely") -eq 0 ]];then
-    ~/bin/drive push -verbose -exclude-ops "delete,update" -no-prompt -destination backup_macsSwig_AJmodels_mfloat/sim_values_AJ_M${MODEL}/${PBS_ID}/ ${OUT_PATH}/${SIM}
-else
-    ~/bin/drive new -folder backup_macsSwig_AJmodels_mfloat/sim_values_AJ_M${MODEL}/${PBS_ID}
-    ~/bin/drive push -verbose -exclude-ops "delete,update" -no-prompt -destination backup_macsSwig_AJmodels_mfloat/sim_values_AJ_M${MODEL}/${PBS_ID}/ ${OUT_PATH}/${SIM}
-fi
+DRIVE_SIM_PATH=backup_macsSwig_AJmodels_mfloat/sim_values_AJ_M${MODEL}/$PBS_ID
+DRIVE_RESULTS_PATH=backup_macsSwig_AJmodels_mfloat/results_sims_AJ_M${MODEL}/$PBS_ID
 
-echo 'google driving ' ${RESULTS}
-if [[ $(~/bin/drive ls backup_macsSwig_AJmodels_mfloat/results_sims_AJ_M${MODEL}/$PBS_ID | grep -q "cannot be found remotely") -eq 0 ]];then
-    ~/bin/drive push -verbose -exclude-ops "delete,update" -no-prompt -destination backup_macsSwig_AJmodels_mfloat/results_sims_AJ_M${MODEL}/${PBS_ID}/ ${OUT_PATH}/${RESULTS}
+echo 'google driving ' ${SIM} 'to '${DRIVE_SIM_PATH}
+if [[ -z "$(~/bin/drive ls ${DRIVE_SIM_PATH})" ]]; then
+    echo ${DRIVE_SIM_PATH} 'is not made yet on google drive'
+    ~/bin/drive new -folder ${DRIVE_SIM_PATH}
 else
-    ~/bin/drive new -folder backup_macsSwig_AJmodels_mfloat/results_sims_AJ_M${MODEL}/${PBS_ID}
-    ~/bin/drive push -verbose -exclude-ops "delete,update" -no-prompt -destination backup_macsSwig_AJmodels_mfloat/results_sims_AJ_M${MODEL}/${PBS_ID}/ ${OUT_PATH}/${RESULTS}
+    echo ${DRIVE_SIM_PATH} 'is already made on google drive'
 fi
+~/bin/drive push -verbose -exclude-ops "delete,update" -no-prompt -destination ${DRIVE_SIM_PATH}/ ${OUT_PATH}/sim_values_AJ_M${MODEL}/${SIM}
+
+echo 'google driving ' ${RESULTS} 'to '${DRIVE_RESULTS_PATH}
+if [[ -z "$(~/bin/drive ls ${DRIVE_RESULTS_PATH})" ]]; then
+    echo ${DRIVE_RESULTS_PATH} 'is not made yet on google drive'
+    ~/bin/drive new -folder ${DRIVE_RESULTS_PATH}
+else
+    echo ${DRIVE_RESULTS_PATH} 'is already made on google drive'
+fi
+~/bin/drive push -verbose -exclude-ops "delete,update" -no-prompt -destination ${DRIVE_RESULTS_PATH}/ ${OUT_PATH}/results_sims_AJ_M${MODEL}/${RESULTS}
 
 
 # backup to Data Store
 echo 'iroding ' ${SIM}
 imkdir -p /iplant/home/agladstein/AJmacs_data/macsSwig_AJmodels_mfloat/sim_values_AJ_M${MODEL}/${PBS_ID}
-iput -K ${OUT_PATH}/${SIM} /iplant/home/agladstein/AJmacs_data/macsSwig_AJmodels_mfloat/sim_values_AJ_M${MODEL}/${PBS_ID}
+iput -K -f ${OUT_PATH}/sim_values_AJ_M${MODEL}/${SIM} /iplant/home/agladstein/AJmacs_data/macsSwig_AJmodels_mfloat/sim_values_AJ_M${MODEL}/${PBS_ID}
 
 echo 'iroding ' ${RESULTS}
 imkdir -p /iplant/home/agladstein/AJmacs_data/macsSwig_AJmodels_mfloat/results_sims_AJ_M${MODEL}/${PBS_ID}
-iput -K ${OUT_PATH}/${RESULTS} /iplant/home/agladstein/AJmacs_data/macsSwig_AJmodels_mfloat/results_sims_AJ_M${MODEL}/${PBS_ID}
+iput -K -f ${OUT_PATH}/results_sims_AJ_M${MODEL}/${RESULTS} /iplant/home/agladstein/AJmacs_data/macsSwig_AJmodels_mfloat/results_sims_AJ_M${MODEL}/${PBS_ID}
 
 
 # rsync to atmosphere
-ssh agladstein@${IP_ADDRESS} mkdir -p /vol_c/results_macsSwig_AJmodels_mfloat/sim_values_AJ_M${MODEL}/${PBS_ID} # test
-echo 'rsyncing ' ${SIM}
-rsync -a ${OUT_PATH}/${SIM} agladstein@${IP_ADDRESS}:/vol_c/results_macsSwig_AJmodels_mfloat/sim_values_AJ_M${MODEL}/${PBS_ID}/${SIM}
+ATMO_SIM_PATH=/vol_c/results_macsSwig_AJmodels_mfloat/sim_values_AJ_M${MODEL}/${PBS_ID}
+ATMO_RESULTS_PATH=/vol_c/results_macsSwig_AJmodels_mfloat/results_sims_AJ_M${MODEL}/${PBS_ID}
 
-ssh agladstein@${IP_ADDRESS} mkdir -p /vol_c/results_macsSwig_AJmodels_mfloat/results_sims_AJ_M${MODEL}/${PBS_ID}
+ssh agladstein@${IP_ADDRESS} mkdir -p ${ATMO_SIM_PATH} # test
+echo 'rsyncing ' ${SIM}
+rsync -a ${OUT_PATH}/sim_values_AJ_M${MODEL}/${SIM} agladstein@${IP_ADDRESS}:${ATMO_SIM_PATH}/${SIM}
+
+ssh agladstein@${IP_ADDRESS} mkdir -p ${ATMO_RESULTS_PATH}
 echo 'rsyncing ' ${RESULTS}
-rsync -a ${OUT_PATH}/${RESULTS} agladstein@${IP_ADDRESS}:/vol_c/results_macsSwig_AJmodels_mfloat/results_sims_AJ_M${MODEL}/${PBS_ID}/${RESULTS}
+rsync -a ${OUT_PATH}/results_sims_AJ_M${MODEL}/${RESULTS} agladstein@${IP_ADDRESS}:${ATMO_RESULTS_PATH}/${RESULTS}
 
 # check that the files are the same on hpc and atmosphere
-echo 'atmo md5sum ' ${SIM} $(ssh agladstein@${IP_ADDRESS} md5sum /vol_c/results_macsSwig_AJmodels_mfloat/sim_values_AJ_M${MODEL}/${PBS_ID}/${SIM} | cut -d " " -f1)
-echo 'hpc md5sum ' ${SIM} $(md5sum ${OUT_PATH}/${SIM} | cut -d " " -f1)
+echo 'atmo md5sum ' ${SIM} $(ssh agladstein@${IP_ADDRESS} md5sum ${ATMO_SIM_PATH}/${SIM} | cut -d " " -f1)
+echo 'hpc md5sum ' ${SIM} $(md5sum ${OUT_PATH}/sim_values_AJ_M${MODEL}/${SIM} | cut -d " " -f1)
 
-if [ $(ssh agladstein@${IP_ADDRESS} md5sum /vol_c/results_macsSwig_AJmodels_mfloat/sim_values_AJ_M${MODEL}/${PBS_ID}/${SIM} | cut -d " " -f1) == $(md5sum ${OUT_PATH}/${SIM} | cut -d " " -f1) ] ; then
-    echo 'rm file ' ${OUT_PATH}/${SIM}
-    rm ${OUT_PATH}/${SIM}
+if [[ $(ssh agladstein@${IP_ADDRESS} md5sum ${ATMO_SIM_PATH}/${SIM} | cut -d " " -f1) == $(md5sum ${OUT_PATH}/sim_values_AJ_M${MODEL}/${SIM} | cut -d " " -f1) ]] ; then
+    echo 'rm file ' ${OUT_PATH}/sim_values_AJ_M${MODEL}/${SIM}
+    rm ${OUT_PATH}/sim_values_AJ_M${MODEL}/${SIM}
 fi
 
-echo 'atmo md5sum ' ${RESULTS} $(ssh agladstein@${IP_ADDRESS} md5sum /vol_c/results_macsSwig_AJmodels_mfloat/sim_values_AJ_M${MODEL}/${PBS_ID}/${RESULTS} | cut -d " " -f1)
-echo 'hpc md5sum ' ${RESULTS} $(md5sum ${OUT_PATH}/${RESULTS} | cut -d " " -f1)
+echo 'atmo md5sum ' ${RESULTS} $(ssh agladstein@${IP_ADDRESS} md5sum ${ATMO_RESULTS_PATH}/${RESULTS} | cut -d " " -f1)
+echo 'hpc md5sum ' ${RESULTS} $(md5sum ${OUT_PATH}/results_sims_AJ_M${MODEL}/${RESULTS} | cut -d " " -f1)
 
-if [ $(ssh agladstein@${IP_ADDRESS} md5sum /vol_c/results_macsSwig_AJmodels_mfloat/sim_values_AJ_M${MODEL}/${PBS_ID}/${RESULTS} | cut -d " " -f1) == $(md5sum ${OUT_PATH}/${RESULTS} | cut -d " " -f1) ] ; then
-    echo 'rm file ' ${OUT_PATH}/${RESULTS}
-    rm ${OUT_PATH}/${RESULTS}
+if [[ $(ssh agladstein@${IP_ADDRESS} md5sum ${ATMO_RESULTS_PATH}/${RESULTS} | cut -d " " -f1) == $(md5sum ${OUT_PATH}/results_sims_AJ_M${MODEL}/${RESULTS} | cut -d " " -f1) ]] ; then
+    echo 'rm file ' ${OUT_PATH}/results_sims_AJ_M${MODEL}/${RESULTS}
+    rm ${OUT_PATH}/results_sims_AJ_M${MODEL}/${RESULTS}
 fi
