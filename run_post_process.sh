fa30dc4e87856e45772bff81a66a4b18669c2185
Ariella Gladstein <agladstein@email.arizona.edu>
2017-05-19 14:13:51 -0700

    don't check if file has already been made

--- run_post_process.sh
+++ run_post_process.sh
@@ -12,11 +12,15 @@ do
     fi
     for BUCKET in $(ls ${IN_PATH}/sim_values_AJ_M${MODEL})
     do
-        if [ ! -f ${OUT_PATH}/input_ABCtoolbox_M${MODEL}_${BUCKET}.txt ]; then
-            echo "python /vol_c/src/macsswig_simsaj/post_process.py ${IN_PATH} ${OUT_PATH} ${MODEL} ${BUCKET} ${HEADER} same &"
-	    python /vol_c/src/macsswig_simsaj/post_process.py ${IN_PATH} ${OUT_PATH} ${MODEL} ${BUCKET} ${HEADER} same &
-        else
-            echo "${OUT_PATH}/input_ABCtoolbox_M${MODEL}_${BUCKET}.txt is already created"
-        fi
+        echo "python /vol_c/src/macsswig_simsaj/post_process.py ${IN_PATH} ${OUT_PATH} ${MODEL} ${BUCKET} ${HEADER} same &"
+        python /vol_c/src/macsswig_simsaj/post_process.py ${IN_PATH} ${OUT_PATH} ${MODEL} ${BUCKET} ${HEADER} same &
+
+        # if it starts to be too slow. do something else fancy to not make files that have already been made.
+#        if [ ! -f ${OUT_PATH}/input_ABCtoolbox_M${MODEL}_${BUCKET}.txt ]; then
+#            echo "python /vol_c/src/macsswig_simsaj/post_process.py ${IN_PATH} ${OUT_PATH} ${MODEL} ${BUCKET} ${HEADER} same &"
+#	        python /vol_c/src/macsswig_simsaj/post_process.py ${IN_PATH} ${OUT_PATH} ${MODEL} ${BUCKET} ${HEADER} same &
+#        else
+#            echo "${OUT_PATH}/input_ABCtoolbox_M${MODEL}_${BUCKET}.txt is already created"
+#        fi
     done
 done
