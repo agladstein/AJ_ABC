505a2787bc20e288182f3cc135081caf28199e50
Ariella Gladstein <agladstein@vm142-42.cyverse.org>
2017-09-15 14:01:59 -0700

    remove lines with missing data

--- subset_stats/main_subset_sim.py
+++ subset_stats/main_subset_sim.py
@@ -20,9 +20,9 @@ def main():
             print 'Creating new file with parameters and summary statistics with correlations passing filter'
             out_file_name = 'pruneCorStats_' + input_file
             if n == 0:
-                input_file_df.drop(input_file_df[stats], axis=1).to_csv(out_file_name, sep='\t', index=False)
+                input_file_df.drop(input_file_df[stats], axis=1).dropna().to_csv(out_file_name, sep='\t', index=False)
             else:
-                input_file_df.drop(input_file_df[stats], axis=1).to_csv(out_file_name, mode = 'a', sep='\t', index=False, header=False)
+                input_file_df.drop(input_file_df[stats], axis=1).dropna().to_csv(out_file_name, mode = 'a', sep='\t', index=False, header=False)
 
         elif option == "keep":
             n_sets = int(argv[4])  # number of sets of statistics from model choice power analysis
@@ -35,9 +35,9 @@ def main():
             out_file_name = 'keepPowerStats_' + input_file
             print 'Creating new file with parameters and summary statistics with high power'
             if n == 0:
-                pd.concat([params_df, input_file_df[stats]], axis=1, join='inner').to_csv(out_file_name, sep='\t', index=False)
+                pd.concat([params_df, input_file_df[stats]], axis=1, join='inner').dropna().to_csv(out_file_name, sep='\t', index=False)
             else:
-                pd.concat([params_df, input_file_df[stats]], axis=1, join='inner').to_csv(out_file_name, mode = 'a', sep='\t', index=False, header=False)
+                pd.concat([params_df, input_file_df[stats]], axis=1, join='inner').dropna().to_csv(out_file_name, mode = 'a', sep='\t', index=False, header=False)
 
         else:
             print "You must specify remove or keep!"
