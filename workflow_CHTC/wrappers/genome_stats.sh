f6cd79ac5c71acf7d73eaacf42ec32157a54567f
Ariella Gladstein <agladstein@email.arizona.edu>
2017-12-07 16:27:07 -0700

    added initial workflow directory for CHTC

---
+++ workflow_CHTC/wrappers/genome_stats.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+MODEL=$1
+JOB_ID=$2
+NUM=$(echo $MODEL | cut -d "_" -f3)
+
+# untar the model code
+tar xzf model.tar.gz
+cd model
+
+mv ../result* .
+mv ../*match .
+
+# set up the venv
+. /cvmfs/oasis.opensciencegrid.org/osg/sw/module-init.sh || true
+module load python/2.7
+# update the venv to the new location
+virtualenv-2.7 workflow/macss_env
+. workflow/macss_env/bin/activate
+
+CMD="python calc_genome_stats_${NUM}.py $JOB_ID ."
+echo
+echo "Running: $CMD"
+$CMD
+
+mv result* ../
+
