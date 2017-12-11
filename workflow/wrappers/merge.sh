739776bc169b36ca73d17feabf900d922b64bf4f
Ariella Gladstein <nu_agladstein@submit-4.chtc.wisc.edu>
2017-12-11 12:31:17 -0600

    fixed merge.sh to include all lines after header

--- workflow/wrappers/merge.sh
+++ workflow/wrappers/merge.sh
@@ -10,7 +10,7 @@ cp $1 $OUTFILE #the first input file becomes the top of the output file
 shift
 # for all the remaining, just the data
 for FILE in "$@"; do
-    tail -n -1 $FILE >>$OUTFILE
+    tail -n +2 $FILE >>$OUTFILE
 done
 
 
