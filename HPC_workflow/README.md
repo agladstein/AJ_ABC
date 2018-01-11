5fcc313970e49ed594af72b3936ffbd679d967f5
Ariella Gladstein <agladstein@email.arizona.edu>
2018-01-11 13:02:19 -0700

    workflow for Ocelote

--- HPC_workflow/README.md
+++ HPC_workflow/README.md
@@ -1,5 +1,32 @@
 # UA HPC Workflow
 
+## Ocelote
+
+Automatically submit parallel whole node jobs until simulation goal is complete.
+
+`checkque_ocelote.sh`
+
+Run `checkque_ocelote.sh` as
+```bash
+/home/u15/agladstein/ABC/macsSwig_AJmodels/checkque_ocelote.sh GOAL QUEMAX VERSION MODEL
+```
+Where `GOAL` is the number of simulations you want to complete,  
+`QUEMAX` is the number of jobs allowed in the que,  
+`VERSION` is the type of simulation (e.g. `instant`, `genome`),  
+`MODEL` is the model number (`1`, `2`, or `3`)
+
+for example:
+```bash
+/home/u15/agladstein/ABC/macsSwig_AJmodels/HPC_workflow/checkque_ocelote.sh 100000 200 genome 1
+```
+In order to run the switch files must be present in the `HPC_workflow` directory,
+```bash
+/home/u15/agladstein/ABC/macsSwig_AJmodels/HPC_workflow/switch1.txt
+/home/u15/agladstein/ABC/macsSwig_AJmodels/HPC_workflow/switch2.txt
+/home/u15/agladstein/ABC/macsSwig_AJmodels/HPC_workflow/switch3.txt
+```
+`checkque_ice.sh` -> `macsargs_model${MODEL}_ocelote_standard.pbs` -> `run-sim_model${MODEL}_ocelote_standard.pbs` 
+
 ## ICE
 
 Automatically submit single core jobs until simulation goal is complete.
@@ -28,17 +55,14 @@ In order to run the switch files must be present in the `HPC_workflow` directory
 /home/u15/agladstein/ABC/macsSwig_AJmodels/HPC_workflow/switch3.txt
 ```
 
-`checkque_ice.sh` -> `main_function_AJmodel_j2.sh` -> `chain_pbs.j2`
+`checkque_ice.sh` -> `main_function_AJmodel_j2.sh` -> `macsargs_pbs.j2`, `run-sim_pbs.j2`, `genome_stats_pbs.j2` -> `macsargs_model${MODEL}_${SYSTEM}_windfall.pbs` -> `run-sim_model${MODEL}_${SYSTEM}_windfall.pbs` -> `genome_stats_model${MODEL}_${SYSTEM}_windfall.pbs`
 
 ### Template PBS
 
 `main_function_AJmodel_j2.sh` uses jinja templates to create the appropriate PBS scripts
 
-There are four jinja templates:  
-`chain_pbs.j2`  
+There are three jinja templates:   
 `macsargs_pbs.j2`  
 `run-sim_pbs.j2`  
 `genome_stats_pbs.j2`
 
-
-## Ocelote
---
+++ HPC_workflow/checkque_ocelote.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+cd /home/u15/agladstein/ABC/macsSwig_AJmodels/HPC_workflow
+
+## This script should only be used on Ocelote
+
+set -e # quits at first error
+
+GOAL=$1
+QUEMAX=$2
+VERSION=genome #$3
+OUT=/rsgrps/mfh4/Ariella/macsSwig_AJmodels_${VERSION}
+MODEL=$4
+
+RESULTS=${OUT}/results_sims_AJ_M${MODEL}
+
+ATMO_DIR=/vol_c/results_macsSwig_AJmodels_${VERSION}/HPC
+
+set -f
+
+if [ -e switch${MODEL}.txt ] ; then
+
+    IP_ADDRESS=$(curl https://gist.githubusercontent.com/agladstein/2bdc122f50314f2a4c7cbc9544e7a325/raw/72953da9732d22dfb1228916d93d90e0af2df639/atmo_instance_ip.txt)
+
+    echo ""
+    echo "#################"
+    date
+    echo "pwd: ${PWD}"
+    echo "Goal: ${GOAL}"
+    echo "Que max: ${QUEMAX} "
+    echo "Version: ${VERSION}"
+    echo "Out path: ${OUT}"
+    echo "Model: ${MODEL}"
+
+    qstat=/cm/shared/apps/pbspro/current/bin/qstat
+    qsub=/cm/shared/apps/pbspro/current/bin/qsub
+
+    #check number of completed simulations
+    echo "Check for ${GOAL} completed runs in $RESULTS"
+    COMP=$(ssh agladstein@${IP_ADDRESS} find ${ATMO_DIR} -type f | wc -l)
+    echo "${COMP} runs have completed"
+
+    if [ "$COMP" -ge "$GOAL" ]; then
+        echo "Goal completed"
+        rm switch${MODEL}.txt
+        echo "Goal completed. ${COMP} runs have completed in $RESULTS." | sendmail agladstein@email.arizona.edu
+        exit
+    else
+        #check number of jobs in que
+        JOBS=$($qstat | grep "agladstein" | cut -d " " -f1)
+        echo $JOBS
+        n=0
+        for j in $JOBS; do
+	        q=$($qstat -t $j | grep -w "Q" | wc -l)
+	        n=$(($n + $q))
+        done
+        echo "You have $n jobs in the que"
+
+        if [ "$n" -ge "$QUEMAX" ]; then
+	        echo "That's enough jobs in the que"
+	        exit
+        else
+	        #create PBS scripts
+#            echo "./main_function_AJmodel_j2.sh ${OUT} ${MODEL}"
+#            ./main_function_AJmodel_j2.sh ${OUT} ${MODEL}
+
+            cd /home/u15/agladstein/ABC
+            echo "rsync -za macsSwig_AJmodels /xdisk/agladstein/macsSwig_AJmodels; cd /xdisk/agladstein/macsSwig_AJmodels"
+            rsync -za macsSwig_AJmodels /xdisk/agladstein/
+            cd /xdisk/agladstein/macsSwig_AJmodels
+
+            echo "Submit to standard"
+            echo "$qsub HPC_workflow/PBS/macsargs_model${MODEL}_ocelote_standard.pbs"
+            $qsub HPC_workflow/PBS/macsargs_model${MODEL}_ocelote_standard.pbs
+        fi
+    fi
+else
+    echo "switch${MODEL}.txt does not exist"
+    exit
+fi
