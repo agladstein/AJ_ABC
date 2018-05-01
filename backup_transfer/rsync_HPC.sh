67ce4ad6462db0b0746fa4cddebec89c24fc2bad
Ariella Gladstein <agladstein@vm142-44.cyverse.org>
2018-05-01 15:34:01 -0700

    HPC data transfer from Ocelote to Atmosphere works

--- backup_transfer/rsync_HPC.sh
+++ backup_transfer/rsync_HPC.sh
@@ -7,14 +7,13 @@ HPC_DIR=/rsgrps/mfh4/Ariella/macsSwig_AJmodels_${VERSION}
 ATMO_DIR=/vol_c/results_macsSwig_AJmodels_${VERSION}/HPC
 
 HOST_NAME=sftp.hpc.arizona.edu
-IP_ADDRESS=$(curl https://gist.githubusercontent.com/agladstein/2bdc122f50314f2a4c7cbc9544e7a325/raw/atmo_instance_ip.txt)
 
 echo "#########################"
 echo "ATMO ${ATMO_DIR}"
 echo "HPC ${HPC_DIR}"
 
+echo "rsync -azv --remove-source-files --exclude '*germline*' --exclude '*sim_data*' --exclude '*ped' --exclude '*map' --exclude '*match' --exclude '*log' --exclude 'macsargs*' agladstein@${HOST_NAME}:${HPC_DIR}/ ${ATMO_DIR}"
+rsync -azv --remove-source-files --exclude '*germline*' --exclude '*sim_data*' --exclude '*ped' --exclude '*map' --exclude '*match' --exclude '*log' --exclude 'macsargs*' agladstein@${HOST_NAME}:${HPC_DIR}/ ${ATMO_DIR}
 
-echo "rsync -az --remove-source-files --exclude '*chr*' --exclude '*ped' --exclude '*map' --exclude '*match' --exclude '*log' --exclude 'macsargs*' agladstein@${HOST_NAME}:${HPC_DIR}/ ${ATMO_DIR}"
-rsync -az --remove-source-files --exclude '*chr*' --exclude '*ped' --exclude '*map' --exclude '*match' --exclude '*log' --exclude 'macsargs*' agladstein@${HOST_NAME}:${HPC_DIR}/ ${ATMO_DIR}
 
 echo "Finished transfer"
